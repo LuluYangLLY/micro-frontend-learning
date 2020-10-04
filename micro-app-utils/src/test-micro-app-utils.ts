5e9bd164fd8ee41ee9ec4fbf65fa1df99311b1b5
lulu.yang <fengshiyunji@foxmail.com>
2020-10-04 23:40:50 +0800

    Create a util module repo

---
+++ micro-app-utils/src/test-micro-app-utils.ts
@@ -0,0 +1,17 @@
+import "./set-public-path";
+
+const isLogin = () => {
+  const getIsLogin = localStorage.getItem("isLogin");
+  return getIsLogin && getIsLogin === "true";
+};
+
+const login = () => {
+  localStorage.setItem("isLogin", "true");
+};
+
+const logout = () => {
+  localStorage.removeItem("isLogin");
+};
+
+// Anything exported from this file is importable by other in-browser modules.
+export { isLogin, login, logout };
